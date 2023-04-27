8ea08aef1190ea4a1c72b31c56d346fba468ff3f
Olga Parfenova <work.parfenova@gmail.com>
2023-04-27 19:31:02 +0300

    redux functionality has been implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchCustomers} from "./asyncActions/customers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const dispatch = useDispatch();
+    const cash = useSelector(state => state.cash.cash);
+    const customers = useSelector(state => state.customers.customers);
 
-export default App;
+    const addCash = (cash) => {
+        dispatch({type: 'ADD_CASH', payload: cash});
+    }
+
+    const getCash = (cash) => {
+        dispatch({type: 'GET_CASH', payload: cash});
+    }
+
+    const addCustomer = (name) => {
+        const customer = {
+            name,
+            id: Date.now(),
+        }
+        dispatch({type: 'ADD_CUSTOMER', payload: customer});
+    }
+
+    const removeCustomer = (customer) => {
+        dispatch({type: 'REMOVE_CUSTOMERS', payload: customer.id});
+    }
+
+    return (
+        <div className={'app'}>
+        <div style={{fontSize: '3rem'}}>{cash}</div>
+        <div style={{display: 'flex'}}>
+            <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+            <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+            <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+            <button onClick={() => dispatch(fetchCustomers())}>Получить клиентов из базы</button>
+        </div>
+        {customers.length > 0 ?
+            <div>
+                {customers.map(customer =>
+                    <div onClick={() => removeCustomer(customer)} style={{fontSize:'2rem', border:'1px solid black', padding:'10px', marginTop:5, cursor:'pointer'}}>{customer.name}</div>
+                )}
+            </div>
+            :
+            <div style={{fontSize:'2rem', marginTop:'20px'}}>
+                Клиенты отсутствуют!
+            </div>
+            }
+        </div>
+    );}
+
+        export default App;
